789d5b104a6c1492f94fef819e083096ecbd579a
JoanLlompart <91556752+JoanLlompart@users.noreply.github.com>
2022-01-30 20:06:06 +0100

    Clase persona

---
+++ Persona.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author joanllompartsocias
+ */
+public class Persona {
+
+    private String nombre;
+    private int edad;
+    protected String DNI;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+   public void setNombre(String nombre) {
+       this.nombre = nombre
+   }
+    
+}
